d4b73efae8193bdb0dcc8a08a7e40c43b195c46e
Mark Philipp <mark.philipp@levvel.io>
2019-05-28 22:01:04 -0400

    Add npm server scripts

--- build/index.js
+++ build/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
+// Watch the build folder
 app.use(express.static('build'));
 var server = app.listen(3000);
 exports.default = server;
